b41e29ca76af89464b36da27930ac8b247e129ce
LukasPio <lucaspio.galvao@gmail.com>
2024-10-08 20:49:13 -0300

    initial commit

---
+++ src/main/java/com/lugusto/ApiDatabaseCourse/ApiDatabaseCourseApplication.java
@@ -0,0 +1,13 @@
+package com.lugusto.ApiDatabaseCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDatabaseCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDatabaseCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lugusto/ApiDatabaseCourse/ApiDatabaseCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugusto.ApiDatabaseCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDatabaseCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
